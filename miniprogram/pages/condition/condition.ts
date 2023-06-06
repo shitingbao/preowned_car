eb6deb67f9165b0f730f34a13ea1306a863ce11f
shitingbao <871258317@qq.com>
2023-06-06 10:44:44 +0800

    perf: other condition

--- miniprogram/pages/condition/condition.ts
+++ miniprogram/pages/condition/condition.ts
@@ -2,21 +2,36 @@
 Page({
     data: {
        "conditions":[
-           {"name":"车型","condition":""},
-           {"name":"品牌","condition":""},
-           {"name":"排放","condition":""},
-           {"name":"价格","condition":""},
-           {"name":"年份","condition":""},
-           {"name":"里程","condition":""},
-           {"name":"颜色","condition":""},
-           {"name":"变速箱","condition":""}
+           {"id":1,"name":"车型","condition":""},
+           {"id":2,"name":"品牌","condition":""},
+           {"id":3,"name":"排放","condition":""},
+           {"id":4,"name":"价格","condition":""},
+           {"id":5,"name":"年份","condition":""},
+           {"id":6,"name":"里程","condition":""},
+           {"id":7,"name":"颜色","condition":""},
+           {"id":8,"name":"变速箱","condition":""}
         ]
     },
   
-    toContent() {
-      wx.navigateTo({
-          url:"./content/content"
-      })
+    toContent(data:any) {
+        wx.navigateTo({
+            url:"./content/content",
+            events: {
+                // 为指定事件添加一个监听器，获取被打开页面传送到当前页面数据
+                acceptDataFromOpenedPage: function(data:any) {
+                    console.log("get content data:",data)
+                },
+                someEvent: function(data:any) {
+                }
+            },
+            success: function(res) {
+                if (data.currentTarget.dataset?.id){
+                    // 通过eventChannel向被打开页面传送数据
+                    res.eventChannel.emit('acceptDataFromOpenerPage', { data: data.currentTarget.dataset.id })
+                }
+                // res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' })
+            }
+        })
     },
     toIndex(){
         wx.navigateBack({
--- miniprogram/pages/condition/content/content.ts
+++ miniprogram/pages/condition/content/content.ts
@@ -12,8 +12,23 @@ Page({
            {"name":"hh","condition":""}
         ]
     },
-    toCondition(){
+    toCondition(data:any){
+        // console.log("content name:",data.currentTarget.dataset.name)
         wx.navigateBack({
+            success:()=>{
+                const eventChannel = this.getOpenerEventChannel()
+                eventChannel.emit('acceptDataFromOpenedPage', {data: data.currentTarget.dataset.name});
+            }
         })
-    }
+    },
+    onLoad: function(option:any){
+        console.log("query=:",option.query)
+        const eventChannel = this.getOpenerEventChannel()
+        // eventChannel.emit('acceptDataFromOpenedPage', {data: 'test'});
+        // eventChannel.emit('someEvent', {data: 'test'});
+        // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+        eventChannel.on('acceptDataFromOpenerPage', function(data) {
+          console.log("get condition data:",data)
+        })
+      }
   });
